e6498322434385d3ff53d17aefcf88ed59f1aad3
Yulovery <yoyomeng2463@163.com>
2017-06-20 11:14:14 +0800

    Images

--- handle.py
+++ handle.py
@@ -35,12 +35,16 @@ class Handle(object):
             webData = web.data()
             print "Handle Post webdata is ", webData   #后台打日志
             recMsg = receive.parse_xml(webData)
-            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+            if isinstance(recMsg, receive.Msg):
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = recMsg.Content
-                #print content, type(content)
-                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                if recMsg.MsgType == 'text':
+                    content = recMsg.Content
+                    #print content, type(content)
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                else if recMsg.MsgType == 'Image':
+                    Images = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, Images)
                 return replyMsg.send()
             else:
                 print "暂且不处理"
--- reply.py
+++ reply.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-s
 # filename: reply.py
 import time
 
