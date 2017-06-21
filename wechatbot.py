3d34041d677bbd42ebef124d696d98d2ac8ac06f
Yulovery <yoyomeng2463@163.com>
2017-06-21 11:46:19 +0800

    pic

--- wechatbot.py
+++ wechatbot.py
@@ -2,6 +2,7 @@
 import werobot
 from werobot.replies import ImageReply
 import requests
+import urllib, cStringIO
 
 robot = werobot.WeRoBot(token='yoyo')
 
@@ -20,7 +21,28 @@ def turing(message):
 # @robot.image 修饰的 Handler 只处理图片消息
 @robot.image
 def img(message):
-    return ImageReply(message=message, media_id=message.MediaId)
+    ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+    # 将256灰度映射到70个字符上
+    def get_char(r,g,b,alpha = 256):
+        if alpha == 0:
+            return ' '
+        length = len(ascii_char)
+        gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+        unit = (256.0 + 1)/length
+        return ascii_char[int(gray/unit)]
+    file = cStringIO.StringIO(urllib.urlopen(message.img).read())
+    im = Image.open(file)
+    print (type (message.img))
+    im = im.resize((24,24), Image.NEAREST)
+    txt = ""
+    for i in range(24):
+        for j in range(24):
+            txt += get_char(*im.getpixel((j,i)))
+        txt += '\n'
+
+    #return ImageReply(message=message, media_id=message.MediaId)
+    print txt
+    return txt
 
 
 # 让服务器监听在 0.0.0.0:80
