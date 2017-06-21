a005f07d573781e51332d6042d2a6f314712090c
Yulovery <yoyomeng2463@163.com>
2017-06-21 10:13:11 +0800

    turing

--- wechatbot.py
+++ wechatbot.py
@@ -7,8 +7,14 @@ robot = werobot.WeRoBot(token='yoyo')
 
 # @robot.text 修饰的 Handler 只处理文本消息
 @robot.text
-def echo(message):
-    return message.content
+def turing(message):
+    url="http://www.tuling123.com/openapi/api"
+    data={
+        'key':'78b9fc3edc4f4ba782bfbeb32497b952',
+        'info':message.content,
+        'userid': '12345678',
+    }
+    return requests.post(url,data=data).json()['text']
 
 # @robot.image 修饰的 Handler 只处理图片消息
 @robot.image
